502a43e98b2485fbfd166b2f6e3715e002acc8f5
Abdus Sobur Shihab <abdussoburshihab56@gmail.com>
2023-02-26 12:31:56 +0600

    model created

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,9 +1,30 @@
 import express from "express"
+import hotel from "../model/hotel.js"
 
 const router = express.Router()
 
 
+//Create
 
+router.post("/", async (req,res)=>{
+
+
+const newHotel = new hotel(req.body)
+
+
+    try{
+        const savedHotel = await newHotel.save()
+        res.status(200).json(savedHotel)
+    }catch(err){
+        res.status(500).json(err)
+    }
+
+})
+
+//Update
+//Delete
+//Get
+//Get All
 
 
 
